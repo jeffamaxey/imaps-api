f551f68a9a61a7ca6f7b7513fb54a6627a87b480
Sam Ireland <sam.ireland.uk@gmail.com>
2020-10-12 14:20:36 +0100

    Start django

---
+++ core/settings.py
@@ -0,0 +1,25 @@
+import os
+from .secrets import SECRET_KEY, BASE_DIR, DATABASES
+
+ALLOWED_HOSTS = []
+
+DEBUG = True
+
+ROOT_URLCONF = "core.urls"
+
+INSTALLED_APPS = [
+    "django.contrib.contenttypes",
+    "django.contrib.staticfiles",
+    "graphene_django",
+    "corsheaders",
+    "core",
+]
+
+MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+]
+
+STATIC_URL = "/static/"
+
+CORS_ORIGIN_ALLOW_ALL = True
