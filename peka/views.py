8849a696e68f81f0f99f57ed562a455a8c4b3e95
Sam Ireland <sam.ireland.uk@gmail.com>
2021-02-03 19:19:38 +0000

    Improve PEKA colors

--- peka/views.py
+++ peka/views.py
@@ -4,4 +4,4 @@ from django.http import JsonResponse
 def data(request):
     with open("peka/data.json") as f:
         data = json.load(f)
-    return JsonResponse(data, safe=False)
+    return JsonResponse(data)
