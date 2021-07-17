b58311006a5f186fe73d4ade867f55befc70384f
Sam Ireland <sam.ireland.uk@gmail.com>
2021-07-17 06:36:54 +0100

    Use PEKA_ROOT for peka data

--- peka/views.py
+++ peka/views.py
@@ -1,5 +1,6 @@
 import json
 from django.http import JsonResponse
+from django.conf import settings
 
 def data(request):
     with open("peka/data/main_heatmap.json") as f:
@@ -10,13 +11,13 @@ def data(request):
         ["noncoding_IDR", "%_noncoding_IDR_peaks"],
         ["total_IDR", "total_IDR_peaks"], ["dendrogram", "dendrogram"]
     ]:
-        with open(f"peka/data/{filename}.json") as f:
+        with open(f"{settings.PEKA_ROOT}/{filename}.json") as f:
             data[key] = json.load(f)
     return JsonResponse(data)
 
 
 def entities(request):
-    with open("peka/data/main_heatmap.json") as f:
+    with open(f"{settings.PEKA_ROOT}/main_heatmap.json") as f:
         data = json.load(f)
     return JsonResponse({
         "proteins": data["columns"], "motifs": data["rows"]
@@ -27,7 +28,7 @@ def rbp(request):
     name = request.GET.get("name")
     if name:
         try:
-            with open(f"peka/data/rbp/{name}.json") as f:
+            with open(f"{settings.PEKA_ROOT}/rbp/{name}.json") as f:
                 return JsonResponse(json.load(f))
         except FileNotFoundError:
             return JsonResponse({"error": "No such RBP"}, status=404)
@@ -37,12 +38,11 @@ def rbp(request):
 def motif(request):
     sequence = request.GET.get("sequence")
     if sequence:
-        with open("peka/data/motif/motif_groups.tsv") as f:
+        with open(f"{settings.PEKA_ROOT}/motif/motif_groups.tsv") as f:
             groups = [l.split("\t") for l in f.read().splitlines()]
         for group in groups:
-            print(group)
             if sequence in group[1].split(", "):
-                with open(f"peka/data/motif/{group[0]}_full.json") as f:
+                with open(f"{settings.PEKA_ROOT}/motif/{group[0]}_full.json") as f:
                     data = json.load(f)
                     data["group"] = group[0]
                     data["group_members"] = group[1].split(", ")
