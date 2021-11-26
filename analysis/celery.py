e44361e697ad8a9c9d0732689fb28212a213d0ff
Sam Ireland <sam.ireland.uk@gmail.com>
2021-11-26 23:33:04 +0000

    Create DataLinks on pipeline completion

--- analysis/celery.py
+++ analysis/celery.py
@@ -6,6 +6,7 @@ import shutil
 from celery import Celery
 from django.conf import settings
 
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("execution")
 app.config_from_object("django.conf:settings", namespace="CELERY")
@@ -15,14 +16,16 @@ app.autodiscover_tasks()
 def run_pipeline(kwargs, job_id):
     """."""
 
-    from django_nextflow.models import Pipeline
-    from analysis.models import Job
+    from django_nextflow.models import Pipeline, Data
+    from analysis.models import DataLink, Job
 
     pipeline = Pipeline.objects.filter(id=kwargs["pipeline"]).first()
     execution = pipeline.run(
         params=json.loads(kwargs["inputs"]),
         data_params=json.loads(kwargs["dataInputs"]),
     )
+    for data in Data.objects.filter(upstream_process_execution__execution=execution):
+        DataLink.objects.create(data=data)
     job = Job.objects.filter(id=job_id).first()
     job.execution = execution
     job.save()
